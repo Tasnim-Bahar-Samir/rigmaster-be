55cf8713dcb96a2cd9cc302d69c91dd94c9b4ac1
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2024-02-28 18:23:49 +0600

    update settings .py

--- RigmasterBackend/settings.py
+++ RigmasterBackend/settings.py
@@ -97,12 +97,24 @@ WSGI_APPLICATION = 'RigmasterBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ["DB_NAME"],  # db name
+            "USER": os.environ["DB_USER"],
+            "PASSWORD": os.environ["DB_PASSWORD"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
