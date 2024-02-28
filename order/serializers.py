20708f2ceececa68c3af539b62528d1c0dcac7be
Al Amin <almin.cont@gmail.com>
2024-02-29 02:29:20 +0600

    fix inventory

--- order/serializers.py
+++ order/serializers.py
@@ -1,5 +1,6 @@
 # model
 from .models import CodOrder, CodPurchaseProduct
+from inventory.models import Size, ProductSizeVarient
 # assets
 # serializer
 from product.serializers import ProductSerializer
@@ -52,7 +53,19 @@ class CodOrderSerializer(serializers.ModelSerializer):
         order = super().create(validated_data)
 
         for i in purchase_order:
-            CodPurchaseProduct.objects.create(order=order, **i)
+            _ordered =  CodPurchaseProduct.objects.create(order=order, **i)
+            product_size_varient = _ordered.others_info.get('product_size_varient', None)
+            if product_size_varient:
+                size_title = product_size_varient.get('size_title', None)
+                quantity = product_size_varient.get('quantity', 0)
+                size = Size.objects.filter(size_title=size_title).first()
+                product_varient = ProductSizeVarient.objects.filter(product=_ordered.product, size=size)
+                if order.status == 'PENDING':
+                    updated_quantity =  product_varient.first().quantity - quantity
+                    product_varient.update(quantity=updated_quantity)
+                elif order.status == 'CANCELLED':
+                    updated_quantity =  product_varient.first().quantity + quantity
+                    product_varient.update(quantity=updated_quantity)
         
         # 
         # sms_text = f"Your order has been confirmed.\n" + \
